4fbc5562f92c489a117dc35b7c5b6e3b599c4138
Gregor Rot <gregor.rot@gmail.com>
2018-12-06 10:59:34 +0100

    Added the deploy.sh script

---
+++ web/deploy.sh
@@ -0,0 +1,17 @@
+cp *.html deploy/
+cp *.css deploy/
+
+cp -ap docs deploy/
+cp -ap media deploy/
+cp -ap software deploy/
+cp -ap tippy deploy/
+
+# obfuscate Javascript
+
+js_files=`ls *.js`
+for fname in $js_files
+do
+  ./node_modules/.bin/javascript-obfuscator $fname --output deploy/$fname
+done
+
+cp -r deploy/* /home/gregor/expressrna/web/
