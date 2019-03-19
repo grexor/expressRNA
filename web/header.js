bf35e914f79af5737f0e599424a60961b0ec260f
Gregor Rot <gregor.rot@gmail.com>
2019-03-19 15:18:33 +0100

    Removed some console.log msgs.

--- web/header.js
+++ web/header.js
@@ -55,7 +55,7 @@ function renderButton() {
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
-    console.log('User signed out.');
+    //console.log('User signed out.');
   });
   $("#btn_signin").show();
   $("#btn_signout").hide();
@@ -79,7 +79,7 @@ function add_history(data, url) {
   if (pop_state==false) {
     if (last_url!=url) {
         window.history.pushState(data, '', url);
-        console.log("add history:"+url+":"+data);
+        //console.log("add history:"+url+":"+data);
       }
     last_url = url;
   }
@@ -92,6 +92,6 @@ window.addEventListener('popstate', function(e) {
     pop_state = false;
     return;
   }
-  console.log("popstate");
+  //console.log("popstate");
   process_login_parameters(e.state["action"], e.state)
 });
