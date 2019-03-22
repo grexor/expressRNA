f55bfbc8f1da70cc057ff43dd825341ce4c06a17
Gregor Rot <gregor.rot@gmail.com>
2019-03-22 13:21:05 +0100

    Removed __init__.py from py/config

--- web/expressrna_py/config/__init__.py
+++
@@ -1,6 +0,0 @@
-mysql_username = "expressrna"
-mysql_password = "krogexpressrna2017"
-mysql_host = "127.0.0.1"
-mysql_database = "expressrna"
-mysql_pool_size = 20
-upload_folder = "/home/gregor/expressrna_dev/web/uploads"
